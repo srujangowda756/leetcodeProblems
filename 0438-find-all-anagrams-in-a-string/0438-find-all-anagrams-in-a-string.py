6c0096e0028f0e876dcabae5f4c01c8181d95732
Srujan K <srujangowda756@gmail.com>
2026-08-19 23:35:37 +0530

    Time: 32 ms (76.47%), Space: 19.7 MB (69.15%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,15 +1,24 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p)>len(s):
+        if len(s) < len(p):
             return []
+
         n=len(p)
-        need=sorted(p)
+        need={}
+        have={}
+        s_len=len(s)
+
+        for i in range(97,97+26):
+            need[chr(i)]=0
+            have[chr(i)]=0
+        for x in range(n):
+            need[p[x]]+=1
+            have[s[x]]+=1
         ans=[]
-        for i in range(len(s)-n+1):
-            if sorted(s[i:i+n])==need:
-                ans.append(i)
+        for ch in range(n-1,len(s)):
+            if have==need:
+                ans.append(ch-n+1)
+            have[s[ch-n+1]]-=1
+            if ch+1<s_len:
+                have[s[ch+1]]+=1
         return ans
-
-
-
-        
